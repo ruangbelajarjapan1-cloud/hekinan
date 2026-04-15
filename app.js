319cd79c6864812789b9afc3dd0168665dd8bcc0
ruangbelajarjapan1-cloud <ruangbelajarjapan1@gmail.com>
2026-04-15 18:08:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ window.addEventListener('error', function(e) {
     }
 }, true); // 'true' penting agar error capture phase tertangkap
 const TARGET_DONASI = 42000000;
-const TERKUMPUL_SAAT_INI = 11625817;
+const TERKUMPUL_SAAT_INI = 11878291;
 
 // Koordinat Hekinan, Jepang
 const HEK_LAT = 34.884;
